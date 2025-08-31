cef8df214f7e26e100dc9d2d8e099cebcffa4ca1
yin753 <101307346+yin753@users.noreply.github.com>
2025-08-31 16:10:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo 'src-git adguardhome https://github.com/kongfl888/luci-app-adguardhome' >> feeds.conf.default
